df533565ab92f3ab153641d00feeb38efb9fcf4a
rupeshkafle <rupeshkafle@gmail.com>
2022-05-05 14:05:40 +0545

    Feat::New(functionality to check for privileged mode and enter if required, functionality to read telnet output line by line)

--- telnet.py
+++ telnet.py
@@ -1,19 +1,17 @@
-from inspect import EndOfBlock
-from queue import Empty
 import sys
 import getpass
 import telnetlib
 import time
-from tkinter import END
 
-
-HOST = "127.0.0.1"
+# HOST = "127.0.0.1"
 USERNAME_PROMPTS = [b"Username:", b"login:"]
 PASSWORD_PROMPTS = [b"Password:", b"password:"]
 LOGGEDIN_PROMPTS = [b">", b"#"]
 FAILED_MSGS = [b"failed", b"incorrect"]
+CHECK_FAILED_MSGS = ["failed", "incorrect"]
 
 loggedin = False
+havemore = False
 host = input("Enter Hostname: ")
 
 while loggedin == False:
@@ -21,17 +19,52 @@ while loggedin == False:
     password = getpass.getpass()
 
     tn = telnetlib.Telnet(host)
-    tn.set_debuglevel(1)
 
     tn.expect(USERNAME_PROMPTS)
     tn.write(username.encode('ascii') + b"\r\n")
     tn.expect(PASSWORD_PROMPTS)
     tn.write(password.encode('ascii') + b"\r\n")
 
-    print(tn.expect(FAILED_MSGS, timeout=1))
+    isFailedObject = tn.expect(FAILED_MSGS, timeout=1)
+    isFailed = isFailedObject[2].decode('ascii')
 
-    # if isFailed:
-    #     print("Authentication Failed")
-    # else:
-    #     tn.expect(LOGGEDIN_PROMPTS)
-    #     tn.write(b"dir\r\n")
+    """ checks if isFailed contains any string which indicates failed logged
+        and displays failure message and returns to start of while loop
+    """
+    if any(key in isFailed for key in CHECK_FAILED_MSGS):
+        print("Authentication Failed")
+    else:
+        loggedin = True
+
+        # Check for  privileged mode
+        if ">" in isFailed:
+            privileged = False
+            tn.write(b"enable\r\n")
+            time.sleep(2)  # wait 2 seconds before going to next code
+            var = tn.read_eager()
+            if b"#" in var:
+                privileged = True
+            while not privileged:
+                enablepassword = input("Enter enable password: ")
+                tn.write(enablepassword.encode('ascii') + b"\r\n")
+                time.sleep(2)
+                var = tn.read_eager()
+                if b"#" in var:
+                    privileged = True
+                    break
+
+tn.write(b"show interface brief\r\n")
+while True:
+    tnResponse = tn.read_until(b"\r\n", 2)
+    print(tnResponse.decode('ascii'))
+    if b"More" in tnResponse:
+        havemore = True
+        while havemore == True:
+            tn.write(b"\r\n")
+            tnResponse = tn.read_until(b"\r\n", 2)
+            print(tnResponse.decode('ascii'))
+            if b"#" in tnResponse:
+                break
+    if b"#" in tnResponse:
+        break
+tn.close()
